d210e6bfd5cb50267bfe8ea4023ce5405190cbf6
Mark Kimsal <metrofindings@gmail.com>
2012-05-15 17:01:02 -0400

    Added sample router and execution timer.

---
+++ src/example/router.php
@@ -0,0 +1,41 @@
+<?php
+class Example_Router {
+
+	public function analyze(&$request) {
+
+		$url = $request->requestedUrl;
+
+		if (strpos($url, '/dologin') === 0) {
+			$request->appUrl  = 'login';
+			$request->appName = 'login';
+//			associate_iCanHandle('authenticate', 'example/authenticator.php');
+		} else {
+			$parts = explode('/', $url);
+			if (isset($parts[1])) {
+				$request->appName = $parts[1];
+				$request->appUrl  = $parts[1];
+				associate_iCanHandle('analyze',       $parts[1].'/main.php');
+				associate_iCanHandle('resources',     $parts[1].'/main.php');
+				associate_iCanHandle('authenticate',  $parts[1].'/main.php');
+				associate_iCanHandle('process',       $parts[1].'/main.php');
+				associate_iCanHandle('output',        $parts[1].'/main.php');
+			}
+		}
+	}
+}
+
+/**
+ * Build a URL the same way this router analyzes one.
+ */
+function r_url($https=0) {
+	static $baseuri;
+	if (!$baseuri) {
+		$baseuri = associate_get('baseuri');
+	}
+
+	if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']== 'on') || $https>0) {
+		return 'https://'.$baseuri;
+	} else {
+		return 'http://'.$baseuri;
+	}
+}
