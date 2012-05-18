726c5a79232dca56b5604fe51439ae9e936f5f2e
Mark Kimsal <metrofindings@gmail.com>
2012-05-17 23:38:30 -0400

    Add m_appurl() function to router so requests to routed apps can be unrouted.

--- src/metrofw/router.php
+++ src/metrofw/router.php
@@ -3,6 +3,7 @@
 class Metrofw_Router {
 
 	public $cycles = 0;
+
 	public function analyze(&$request) {
 
 		if ($request->requestedUrl == '' && $this->cycles == 0) {
@@ -14,22 +15,39 @@ class Metrofw_Router {
 
 		$url = $request->requestedUrl;
 		if (strpos($url, '/dologin') === 0) {
-			$request->moduleUrl  = 'login';
-			$request->moduleName = 'login';
+			$request->appUrl  = 'login';
+			$request->appName = 'login';
 			associate_iCanHandle('authenticate', 'metrou/authenticator.php');
 			associate_iCanOwn('output', 'metrofw/redir.php');
-		} else {
-			$parts = explode('/', $url);
-			if (isset($parts[1])) {
-				$request->moduleName = $parts[1];
-				$request->moduleUrl  = $parts[1];
-				associate_iCanHandle('analyze',  $parts[1].'/main.php');
-				associate_iCanHandle('resources',  $parts[1].'/main.php');
-				associate_iCanHandle('authenticate',  $parts[1].'/main.php');
-				associate_iCanHandle('process',  $parts[1].'/main.php');
-				associate_iCanHandle('output',  $parts[1].'/main.php');
-			}
+			return;
 		}
+
+		$parts = explode('/', $url);
+		if (!isset($parts[1])) {
+			$parts[1] = 'main';
+		}
+		$request->appName = $parts[1];
+		$request->appUrl  = $parts[1];
+		associate_iCanHandle('analyze',  $parts[1].'/main.php');
+		associate_iCanHandle('resources',  $parts[1].'/main.php');
+		associate_iCanHandle('authenticate',  $parts[1].'/main.php');
+		associate_iCanHandle('process',  $parts[1].'/main.php');
+		associate_iCanHandle('output',  $parts[1].'/main.php');
+	}
+
+	public function unrouteUrl($app) {
+		return $app;
+	}
+
+	public function formatArgs($args) {
+		if ($args === NULL) {
+			return '';
+		}
+		$v = '';
+		foreach ($args as $_k => $_v) {
+			$v .= '/'.urlencode($_k).'='.urlencode($_v);
+		}
+		return $v;
 	}
 }
 
@@ -46,5 +64,31 @@ function m_url($https=0) {
 		return 'https://'.$baseuri;
 	} else {
 		return 'http://'.$baseuri;
+	}
+}
+
+/**
+ */
+function m_appurl($url, $args=null, $https=-1) {
+	static $baseUri;
+	static $templateName;
+	static $templatePath;
+	if (!$baseUri) {
+		$baseUri = associate_get('baseuri');
+	}
+
+	$router = associate_getMeA('router');
+	// *
+	$url  = $router->unrouteUrl($url);
+	$url .= $router->formatArgs($args);
+	$end  = $baseUri.$url.'/';
+	// * /
+
+	if ($https === 0) {
+		return 'http://'.$end;
+	} else if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || $https>0) {
+		return 'https://'.$end;
+	} else {
+		return 'http://'.$end;
 	}
 }
