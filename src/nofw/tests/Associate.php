8f3dda28f10fb29c3017a14e943878bf0d66e659
Mark Kimsal <metrofindings@gmail.com>
2013-08-11 19:28:57 -0400

    Add empty handler for testing.

--- src/nofw/tests/Associate.php
+++ src/nofw/tests/Associate.php
@@ -1,11 +1,13 @@
 <?php
 
-class Nofw_Test_Associate extends UnitTestCase { 
+include_once('src/nofw/associate.php');
+include_once('src/nofw/proto.php');
 
+class Nofw_Tests_Associate extends UnitTestCase { 
 
 	public function setUp() {
-		include_once('src/nofw/associate.php');
 	}
+
 	public function test_SkipBadHandlers() {
 		_iCanHandle('testphase1', 'non/existant.php');
 		_iCanHandle('testphase1', 'non/existant2.php');
@@ -15,26 +17,45 @@ class Nofw_Test_Associate extends UnitTestCase {
 		}
 	}
 
-	public function test_FindComponentHandlers() {
-		_iCanHandle('testphase2', 'nofw-simpletest/emptyhandler.php');
+	/**
+	 * Ensure that iCanHandle produces the correct number of handler objects
+	 */
+	public function test_FindHandlers() {
+		_iCanHandle('testphase2', 'nofw/tests/emptyhandler.txt');
+		_iCanHandle('testphase2', 'nofw/tests/emptyhandler.txt');
 		$a = Nofw_Associate::getAssociate();
+		$count = 0;
+		$prevsvc = array();
 		while ($svc = $a->whoCanHandle('testphase2')) {
-			$this->assertTrue( is_object($svc[0]) );
+			$prevsvc = $svc;
+			++$count;
 		}
+		$this->assertTrue( is_object($prevsvc[0]) );
+		$this->assertEqual( 2, $count );
 	}
 
+	/**
+	 * Ensure you can pass an existing object reference as
+	 * a lifecycle handler
+	 */
 	public function test_UseRealObjectsAsHandlers() {
-		_iAmA('emptyhandler', 'nofw-simpletest/emptyhandler.php');
+		_iAmA('emptyhandler', 'nofw/tests/emptyhandler.txt');
 		$obj = _getMeA('emptyhandler');
 		_iCanHandle('testphase3', $obj);
 		$a = Nofw_Associate::getAssociate();
+		$prevsvc = array();
 		while ($svc = $a->whoCanHandle('testphase3')) {
-			$this->assertTrue( is_object($svc[0]) );
+			$prevsvc = $svc;
 		}
+		$this->assertTrue( is_object($prevsvc[0]) );
 	}
 
+	/**
+	 * Ensure you can pass a user call back array as a lifecycle 
+	 * handler
+	 */
 	public function test_UseArrayCallbackAsHandlers() {
-		_iAmA('emptyhandler', 'nofw-simpletest/emptyhandler.php');
+		_iAmA('emptyhandler', 'nofw/tests/emptyhandler.txt');
 		$obj = _getMeA('emptyhandler');
 		_iCanHandle('testphase4', array(&$obj, 'dummyfunc'));
 		$a = Nofw_Associate::getAssociate();
