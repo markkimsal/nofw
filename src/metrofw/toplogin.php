79421edbb15772bdb446f0e01a24a0a57b2a7783
Mark Kimsal <metrofindings@gmail.com>
2013-09-10 09:58:42 -0400

    Moving metrofw to it's own repo

--- src/metrofw/toplogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Metrofw_Toplogin {
-
-	public function template(&$req) {
-		return '<a href="'.m_appurl('login').'">login</a>';
-	}
-}
--- src/metrofw/utils.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Metrofw_Utils {
-
-	public function resources(&$req) {
-	//	associate_set('baseuri', $req->baseUri);
-	}
-}
-
